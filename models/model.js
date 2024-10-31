de0a5a28974edfbb6c744191c0d0d8440204289b
Jowie Ardinata <jowieardinata@gmail.com>
2024-10-31 23:34:16 +0700

    mengganti library bcrypt dengan bcryptjs

--- models/model.js
+++ models/model.js
@@ -1,7 +1,7 @@
 import { Sequelize,DataTypes } from "sequelize";
 
-const sequelize = new Sequelize('artileg', 'root','', {
-    host: 'localhost',
+const sequelize = new Sequelize('mydatabase', 'myuser','mypassword', {
+    host: 'mysql',
     dialect: 'mysql'
 })
 
