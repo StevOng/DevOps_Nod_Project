2799818fd56f46c637546b49d0f4904fbcffdee7
StevOng <130033827+StevOng@users.noreply.github.com>
2024-11-04 15:09:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
-import express from "express";
-import session from "express-session";
-import { sequelize } from "./models/model.js";
-import userRoutes from "./models/routers/user.js";
+import express from 'express'
+import session from 'express-session'
+import { sequelize } from './models/model.js'
+import userRoutes from './models/routers/user.js'
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 app.use(
   session({
-    secret: "Ini adalah kode secret###",
+    secret: 'Ini adalah kode secret###',
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 60 * 60 * 1000 },
+    cookie: { maxAge: 60 * 60 * 1000 }
   })
-);
-app.use(express.static("views"));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.static("views/js/model"));
-app.set("view engine", "ejs");
+)
+app.use(express.static('views'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('views/js/model'))
+app.set('view engine', 'ejs')
 
-app.use("/", [userRoutes]);
+app.use('/', [userRoutes])
 
-app.get("/", (req, res) => {
-  // let result = await User.findOne({where: {email: req.body.email}})
-  res.render("home", { user: req.session.user });
-});
+app.get('/', (req, res) => {
+  res.render('home', { user: req.session.user })
+})
 
-app.get("/connect", (req, res) => {
+app.get('/connect', (req, res) => {
   try {
-    sequelize.authenticate();
-    res.end("Connected");
+    sequelize.authenticate()
+    res.end('Connected')
   } catch (err) {
-    res.end("Disconnected");
+    res.end('Disconnected')
   }
-});
+})
 
-app.get("*", (req, res) => {
-  res.render("404");
-});
+app.get('*', (req, res) => {
+  res.render('404')
+})
 
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
-);
+)
