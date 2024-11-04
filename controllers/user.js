d7012cf3d1c02bb7faab4337b1a08535159adb9a
StevOng <130033827+StevOng@users.noreply.github.com>
2024-11-04 14:41:43 +0700

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,103 +1,100 @@
-import User from "../models/users.js"
+import User from '../models/users.js'
 import bcrypt from 'bcryptjs'
 
-const login = (req,res,next) => {
-    let msg = req.session.err || ""
-    req.session.err = ""
-    if(req.session.user) {
+const login = (req, res, next) => {
+  const msg = req.session.err || ''
+  req.session.err = ''
+  if (req.session.user) {
+    res.redirect('/')
+  } else {
+    res.render('login', { user: req.session.user || '', message: msg })
+  }
+}
+
+const logout = (req, res, next) => {
+  req.session.destroy()
+  res.redirect('/login')
+}
+
+const auth = (req, res, next) => {
+  const data = {
+    email: req.body.email,
+    password: req.body.password
+  }
+  req.session.err = ''
+  User.findOne({ where: { email: data.email } }).then(result => {
+    if (!result) {
+      req.session.err = 'Incorrect email or password'
+      res.redirect('/login')
+    } else {
+      const validPass = bcrypt.compareSync(data.password, result.password)
+      if (!validPass) {
+        req.session.err = 'Incorrect password'
+        res.redirect('/login')
+      } else {
+        req.session.err = ''
+        req.session.user = result
         res.redirect('/')
-    } else {
-        res.render('login', {user: req.session.user || "", message: msg})
+      }
     }
+  })
 }
 
-const logout = (req,res,next) => {
-    req.session.destroy()
-    res.redirect('/login')
+const register = (req, res, next) => {
+  const err = req.session.err || ''
+  req.session.user = ''
+  res.render('register', { msg: err })
 }
 
-const auth = (req,res,next) => {
-    const data = {
-        email: req.body.email,
-        password: req.body.password
-    }
-    req.session.err = ""
-    User.findOne({where: {email: data.email}}).then(result => {
-        if(!result) {
-            req.session.err = "Incorrect email or password"
-            res.redirect('/login')
-        }
-        else {
-            let validPass = bcrypt.compareSync(data.password, result.password)
-            if(!validPass) {
-                req.session.err = "Incorrect password"
-                res.redirect('/login')
-            }
-            else {
-                req.session.err = ""
-                req.session.user = result
-                res.redirect('/')
-            }
-        }
-    }) 
-}
-
-const register = (req,res,next) => {
-    let err = req.session.err || ""
-    req.session.user = ""
-    res.render('register',{msg: err})
-}
-
-const regist = async(req,res,next) => {
-    let myPass = req.body.password
-    let hashed = await bcrypt.hash(myPass,10)
-    if(myPass.length < 4){
-        req.session.err = "Password must at least 4 character"
-        res.redirect('/register')
-    }
-    else {
-        let result = await User.create({
-            email: req.body.email,
-            username: req.body.username,
-            password: hashed
-        })
-        req.session.err = ""
-        res.redirect('/login')
-    }
-}
-
-const profile = (req,res,next) => {
-    User.findAll().then(() => {
-        res.render('profile', {user: req.session.user ||""})
+const regist = async (req, res, next) => {
+  const myPass = req.body.password
+  const hashed = await bcrypt.hash(myPass, 10)
+  if (myPass.length < 4) {
+    req.session.err = 'Password must at least 4 characters'
+    res.redirect('/register')
+  } else {
+    await User.create({
+      email: req.body.email,
+      username: req.body.username,
+      password: hashed
     })
-}
-
-const accDel = async(req,res,next) => {
-    await User.destroy({where: {id: req.session.user.id}})
-    req.session.user = ""
+    req.session.err = ''
     res.redirect('/login')
+  }
 }
 
-const renEdit = (req,res,next) => {
-    let errs = req.session.err || ""
-    res.render('edit',{msg: errs, user: req.session.user||""}) 
+const profile = (req, res, next) => {
+  User.findAll().then(() => {
+    res.render('profile', { user: req.session.user || '' })
+  })
 }
 
-const accEdit = async(req,res,next) => {
-    let myPass = req.body.password
-    let hashed = await bcrypt.hash(myPass,10)
-    if(myPass.length < 4){
-        req.session.err = "Password must at least 4 character"
-        res.redirect('/edit')
-    } else {
-        await User.update({
-            email: req.body.email,
-            username: req.body.username,
-            password: hashed
-        }, {where: {id: req.session.user.id}})
-        req.session.user = ""
-        res.redirect('/login')
-    }
+const accDel = async (req, res, next) => {
+  await User.destroy({ where: { id: req.session.user.id } })
+  req.session.user = ''
+  res.redirect('/login')
 }
 
-export default {login,logout,auth,regist,register,profile,accDel,renEdit,accEdit}
+const renEdit = (req, res, next) => {
+  const errs = req.session.err || ''
+  res.render('edit', { msg: errs, user: req.session.user || '' })
+}
+
+const accEdit = async (req, res, next) => {
+  const myPass = req.body.password
+  const hashed = await bcrypt.hash(myPass, 10)
+  if (myPass.length < 4) {
+    req.session.err = 'Password must at least 4 characters'
+    res.redirect('/edit')
+  } else {
+    await User.update({
+      email: req.body.email,
+      username: req.body.username,
+      password: hashed
+    }, { where: { id: req.session.user.id } })
+    req.session.user = ''
+    res.redirect('/login')
+  }
+}
+
+export default { login, logout, auth, regist, register, profile, accDel, renEdit, accEdit }
