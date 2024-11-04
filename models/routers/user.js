4322705935822773ccae172dc42c3c5c93ec057f
StevOng <130033827+StevOng@users.noreply.github.com>
2024-11-04 15:28:15 +0700

    Update user.js

--- models/routers/user.js
+++ models/routers/user.js
@@ -4,27 +4,27 @@ import userAuth from '../../controllers/user.js'
 
 const router = express.Router()
 
-router.get('/login',userAuth.login)
-router.post('/login',userAuth.auth)
+router.get('/login', userAuth.login)
+router.post('/login', userAuth.auth)
 
-router.get('/logout',userAuth.logout)
+router.get('/logout', userAuth.logout)
 
-router.get('/register',userAuth.register)
-router.post('/register',userAuth.regist)
+router.get('/register', userAuth.register)
+router.post('/register', userAuth.regist)
 
 router.get('/profile', userAuth.profile)
 router.get('/delete', userAuth.accDel)
 router.get('/edit', userAuth.renEdit)
 router.post('/edit', userAuth.accEdit)
 
-router.get('/create_user_db', (req,res) => {
-    User.sync({alter:true})
-    res.end('User table create')
+router.get('/create_user_db', (req, res) => {
+  User.sync({ alter: true })
+  res.end('User table create')
 })
 
-router.get('/dropUserDb', (req,res) => {
-    User.drop()
-    res.end('User table remove')
+router.get('/dropUserDb', (req, res) => {
+  User.drop()
+  res.end('User table remove')
 })
 
 export default router
